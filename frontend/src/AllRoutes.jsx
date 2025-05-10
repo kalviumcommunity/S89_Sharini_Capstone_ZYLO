73bb86aab78361354581e8e8255c5c5179ae19d5
chitti239 <sharinis239@gmail.com>
2025-05-10 20:11:47 +0530

    created components folder

--- frontend/src/AllRoutes.jsx
+++ frontend/src/AllRoutes.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Homepage from './Components/Homepage'
-import Chatpage from './Components/Chatpage'
-import Lumeno from './Components/lumeno'
 import UsrProfile from './Components/UsrProfile'
 
 const AllRoutes = () => {
   return (
     <Routes>
         <Route path="/" element={<Homepage/>} />
-        <Route path="/chat/:userId" element={<Chatpage/>} />
-        <Route path='/lumeo' element={<Lumeno/>} />
         <Route path='/userProfile' element={<UsrProfile/>} />
     </Routes>
   )
