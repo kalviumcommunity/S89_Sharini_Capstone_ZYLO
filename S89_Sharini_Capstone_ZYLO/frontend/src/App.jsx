993290af7abb1493a2be2d63572e6c5a968efce2
chitti239 <sharinis239@gmail.com>
2025-05-16 15:05:41 +0530

    done

---
+++ S89_Sharini_Capstone_ZYLO/frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import AllRoutes from './AllRoutes'
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+
+
+  return (
+    <>
+    <BrowserRouter>
+    <AllRoutes/>
+    </BrowserRouter>
+     </>
+  )
+}
+
+export default App
