21d8cbc1da255e0ce714904ee54d8be7755e4e34
chitti239 <sharinis239@gmail.com>
2025-05-16 11:53:19 +0530

    Added componemts folder

--- frontend/src/AllRoutes.jsx
+++ frontend/src/AllRoutes.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import UsrProfile from './Components/UserProfile'
 import Homepage from './Components/Homepage'
-import UsrProfile from './Components/UsrProfile'
 
 const AllRoutes = () => {
   return (
